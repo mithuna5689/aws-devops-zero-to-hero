0fbc1fb37ab97257292ef5f5fec215f9d46f8843
mithuna5689 <mithuitsme5689@gmail.com>
2024-07-31 14:09:50 +0530

    make changes

--- day-14/simple-python-app/stop_container.sh
+++ day-14/simple-python-app/stop_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-containerid = 'docker ps | awk -F " " '{print $1}''
-docker rm -f $containerid
+# Stop all running containers
+docker ps -q | xargs docker rm -f
+
